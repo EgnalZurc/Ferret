e7ae6cbf5dc1bf7f9382aee1f5df01402f2bc462
unknown <acmlsn@gmail.com>
2019-02-10 14:58:37 +0100

    Action object. It contains action data and execute user action

---
+++ src/main/java/com.ferret/Action.java
@@ -0,0 +1,81 @@
+package com.ferret;
+
+import com.jsoniter.annotation.JsonProperty;
+
+public class Action {
+    public static final int COUNTLINES = 0;
+    public static final int GETLINE = 1;
+    public static final int ADDLINE = 2;
+    public static final int REPLACELINE = 3;
+    public static final int GETTEXT = 4;
+    public static final int INSERTLINE = 5;
+    public static final int DELETELINE = 6;
+    public static final int CREATEFILE = 7;
+    public static final int SEARCHSUBSTR = 8;
+    @JsonProperty(nullable = false)
+    public int actionID;
+    @JsonProperty(nullable = false)
+    public String fileName;
+    @JsonProperty(nullable = true)
+    public int line = -1;
+    @JsonProperty(nullable = true)
+    public String text = null;
+
+    public String doAction() {
+        FileHandler fileHandler = new FileHandler();
+        switch (actionID) {
+            case COUNTLINES:
+                return fileHandler.countLines(fileName);
+            case GETLINE:
+                if(line > 0) {
+                    return fileHandler.getLine(fileName, line);
+                } else {
+                    return "Incorrect line asked in get line action";
+                }
+            case ADDLINE:
+                if(text != null) {
+                    return fileHandler.addLine(fileName, text);
+                } else {
+                    return "Incorrect text selected";
+                }
+            case REPLACELINE:
+                if(text != null && line > 0) {
+                    return fileHandler.replaceLine(fileName, text, line);
+                } else {
+                    if(text != null) {
+                        return "Incorrect text selected";
+                    } else if(line > 0) {
+                        return "Incorrect line asked in get line action";
+                    }
+                }
+            case GETTEXT:
+                return fileHandler.getText(fileName);
+            case INSERTLINE:
+                if(text != null && line > 0) {
+                    return fileHandler.insertLine(fileName, text, line);
+                } else {
+                    if(text != null) {
+                        return "Incorrect text selected";
+                    } else if(line > 0) {
+                        return "Incorrect line asked in get line action";
+                    }
+                }
+            case DELETELINE:
+                if(line > 0) {
+                    return fileHandler.deleteLine(fileName, line);
+                } else {
+                    return "Incorrect line asked in get line action";
+                }
+            case CREATEFILE:
+                return fileHandler.createFile(fileName);
+            case SEARCHSUBSTR:
+                if(text != null) {
+                    return fileHandler.searchSubStr(fileName, text);
+                } else {
+                    return "Incorrect word to be found";
+                }
+            default:
+            return "Incorrect acion selected";
+        }
+    }
+}
