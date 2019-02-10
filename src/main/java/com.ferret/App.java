0364c7bd92ad62fa95d94b5c5a2b7aefa64cafe7
unknown <acmlsn@gmail.com>
2019-02-10 14:57:39 +0100

    main class

--- src/main/java/com.ferret/App.java
+++ src/main/java/com.ferret/App.java
@@ -3,13 +3,7 @@ package com.ferret;
 import com.blade.*;
 
 public class App {
-
     public static void main(String[] args) {
-        Blade blade = Blade.me();
-        Blade.of().post("/ferret", ctx -> {
-            // ctx.bodyToString() prints the post text
-            ctx.html("caca");
-        }).start();
-        Blade.of().listen(9001).start();
+        Blade.of().start(App.class, args);
     }
 }
