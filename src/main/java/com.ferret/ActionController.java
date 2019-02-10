6cff4ea89b0f44b76ed90fb7ef2eb57ee257bb68
unknown <acmlsn@gmail.com>
2019-02-10 14:59:23 +0100

    post API

---
+++ src/main/java/com.ferret/ActionController.java
@@ -0,0 +1,21 @@
+package com.ferret;
+
+import com.blade.mvc.annotation.*;
+import com.blade.mvc.ui.RestResponse;
+import com.blade.mvc.RouteContext;
+import com.jsoniter.JsonIterator;
+
+@Path("")
+public class ActionController {
+
+    @PostRoute("/ferret")
+    @JSON
+    public RestResponse doAction(@Param String request){
+        try {
+            Action action = JsonIterator.deserialize(request, Action.class); 
+            return RestResponse.ok(action.doAction());
+        } catch (Exception e) {
+            return RestResponse.ok(e.toString());
+        }
+    }
+}
